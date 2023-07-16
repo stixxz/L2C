49a349f9c730f2abce6ccc02a7915863420bcfea
TeamStixxz <m.terlouw15@upcmail.nl>
2023-07-16 23:19:33 +0200

    update 16-7-2023 0.1
    unity les 1.2 & 1.3 & 1.4 klaar

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float turnSpeed = 30.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +17,15 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
+        
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
     }
 }
