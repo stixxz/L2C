43e2038c4d1e09aa023eced74465140aef8e10de
TeamStixxz <m.terlouw15@upcmail.nl>
2023-07-17 20:45:58 +0200

    Prototyp1 uni 1 compleet
    hoofdstuk 1 unity course compleet volgende push zal start zijn aan hoofdstuk 2

--- Prototype1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 0.5f;
+    public float rotationSpeed = 100.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -15,15 +15,15 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
